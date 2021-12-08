ea59d6893254cf79e4c9b71ef491e62114fe5b6e
Martin Svanberg <martin.svanberg@northvolt.com>
2021-12-08 20:46:25 +0100

    Day 7

---
+++ days/07/a/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/martin-svanberg-northvolt/aoc21/lib"
+)
+
+func main() {
+	input := lib.StringsToInts(strings.Split(lib.GetInput()[0], ","))
+	minFuel := math.MaxInt
+	for _, i := range input {
+		totalFuel := 0
+		for _, align := range input {
+			totalFuel += abs(align - i)
+		}
+		if totalFuel < minFuel {
+			minFuel = totalFuel
+		}
+	}
+	fmt.Print(minFuel)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
