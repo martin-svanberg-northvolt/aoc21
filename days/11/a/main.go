781823a01333c984b53e7e2355e21a8c28c45186
Martin Svanberg <martin.svanberg@northvolt.com>
2021-12-11 14:23:33 +0100

    Day 11

---
+++ days/11/a/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/martin-svanberg-northvolt/aoc21/lib"
+)
+
+type point struct {
+	X, Y int
+}
+
+func main() {
+	input := lib.GetInput()
+	energies := make([][]int, 0)
+	flashes := 0
+	for _, row := range input {
+		energies = append(energies, lib.StringsToInts(strings.Split(row, "")))
+	}
+	for step := 0; step < 100; step++ {
+		for i, row := range energies {
+			for j := range row {
+				energies[i][j] += 1
+			}
+		}
+		flashedAt := make(map[point]bool)
+		anyFlashes := true
+		for anyFlashes {
+			anyFlashes = false
+			for y, row := range energies {
+				for x, energy := range row {
+					point := point{X: x, Y: y}
+					if energy > 9 && !flashedAt[point] {
+						incAdjacent(energies, x, y)
+						flashes += 1
+						flashedAt[point] = true
+						anyFlashes = true
+					}
+				}
+			}
+		}
+		for y, row := range energies {
+			for x, energy := range row {
+				if energy > 9 {
+					energies[y][x] = 0
+				}
+			}
+		}
+	}
+	fmt.Println(flashes)
+}
+
+func incAdjacent(energies [][]int, x, y int) {
+	adjacent := [][]int{
+		{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1},
+		{x - 1, y}, {x + 1, y},
+		{x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1},
+	}
+	width, height := len(energies[0]), len(energies)
+	for _, a := range adjacent {
+		ax, ay := a[0], a[1]
+		if ax >= 0 && ax < width && ay >= 0 && ay < height {
+			energies[ay][ax] += 1
+		}
+	}
+}
